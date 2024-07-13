295e75aa2e66a69f42edb59fabce042bd17670ea
anas11abdo <anas.abdo.19999@gmail.com>
2024-07-13 21:25:26 +0300

    last commit

--- cypress/Pageobjects/createBoard/assertions.cy.js
+++
@@ -1,9 +0,0 @@
-class CreateBoardAssertion{
-    checkBoardNameContainValue(){
-        cy.origin("https://trello.com",()=>{
-            cy.get('[data-testid="board-name-display"]').should('contain','Anas First Board').and('be.visible');
-            });
-    return this;
-    }
-    }
-    export default CreateBoardAssertion;
--- cypress/Pageobjects/createList/assertions.cy.js
+++
@@ -1,9 +0,0 @@
-class CreateListAssertion{
-    checkListNameContainValue(){
-        cy.origin("https://trello.com",()=>{
-            cy.get('[data-testid="list-name"]').should('contain','Anas First List').and('be.visible');
-            });
-    return this;
-    }
-    }
-    export default CreateListAssertion;
